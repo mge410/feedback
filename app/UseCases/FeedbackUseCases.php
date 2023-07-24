b9c4839dfdabb58c08cc67cfa9e234b257c3952d
Vladimir <v0v.voron2005@yandex.ru>
2023-07-25 00:05:39 +0300

    Update AcceptFeedbackData.php and FeedbackUseCases.php

--- app/UseCases/FeedbackUseCases.php
+++ app/UseCases/FeedbackUseCases.php
@@ -14,6 +14,8 @@ class FeedbackUseCases
         $feedback->title = $data->title;
         $feedback->description = $data->description;
         $feedback->datetime = $data->datetime->format('Y-m-d H:i:s');
+        $feedback->service_id = (integer) $data->service_id;
+        $feedback->rating = (integer) $data->rating;
         $feedback->save();
         $feedback->refresh();
 
